aa73083e93594ab16605a5daed7ffd34979a6540
fcastiglioni <fcastiglioni@correo.um.edu.uy>
2020-12-15 14:22:00 -0300

    4th commit <video 26>

---
+++ platziverse-db/setup.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const debug = require('debug')('platziverse:db:setup')
+const db = require('./')
+
+const prompt = inquirer.createPromptModule()
+
+async function setup () {
+
+  if (process.argv[2] == "y" || process.argv[2] == "Y" ){
+    const config = {
+      database: process.env.DB_NAME || 'platziverse',
+      username: process.env.DB_USER || 'platzi',
+      password: process.env.DB_PASS || 'platzi',
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres',
+      loggin: s => debug(s),
+      setup: true
+    }
+  
+    await db(config).catch(handleFatalError)
+  
+    console.log('Success!')
+    process.exit(0)
+  }
+
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy the database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log(`${chalk.yellow('no')}`+`${chalk.red('thi')}`+`${chalk.green('ng')}`+ `${chalk.blueBright(' happened ')}`+`${chalk.magentaBright(' :)')}`)
+  }
+
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    loggin: s => debug(s),
+    setup: true
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+setup()
